adbd100c3f0708af2aaa3f6e3539ebafa5bc5134
Himanshu Dewangan <himanshu.dewangan@yahoo.com>
2022-11-09 00:24:51 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'zenda-app';
+
+  displayForm: boolean = false;
+
+  profileForm = new FormGroup({
+    phoneNumber: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[+91]{3}[0-9]{10}')//first 3 must be +91 and rest 10 digits, maxlength is 10 type tel
+    ]),
+    panNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10)
+    ]),
+    name: new FormControl('', [
+      Validators.required
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+  });
+
+  toggleForm() {
+    this.displayForm = !this.displayForm;
+  }
+
+  onSubmit() {
+    alert('Saved Sucessfully');
+    this.toggleForm();
+  }
+
 }
